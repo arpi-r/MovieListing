cbf050780f2a6228e6006d0721d079f2843f4f7e
Arpitha Raghunandan <arpitharaghunandan@gmail.com>
2020-04-02 15:49:55 +0530

    Update improving search

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -3,12 +3,24 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
+  search_src = "https://api.themoviedb.org/4/search/movie?api_key=dea94f82d808e85759059b08ab481c70&query="
+
+  httpGet(theUrl)
+  {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+      xmlHttp.send( null );
+      return xmlHttp.responseText;
+  }
+
   transform(items: any[], searchText: string): any[] {
     if(!items) return [];
     if(!searchText) return items;
-    searchText = searchText.toLowerCase();
-    return items.filter( it => {
-      return it.title.toLowerCase().includes(searchText);
-    });
+
+    
+    var data = this.httpGet(this.search_src + searchText);
+    var temp = JSON.parse(data).results;
+
+    return temp;
    }
 }
